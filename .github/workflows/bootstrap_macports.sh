6394465899650a3a3955ef713390dad16f0c7e93
nmlynch94 <40608755+nmlynch94@users.noreply.github.com>
2024-01-11 00:47:34 -0500

    Update bootstrap_macports.sh

--- .github/workflows/bootstrap_macports.sh
+++ .github/workflows/bootstrap_macports.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# https://github.com/macports/macports-ports/blob/master/.github/workflows/bootstrap.sh
-# The above link is the original source of this script before editing
-# This worked better then the github action for installing Macports
-
 set -e
+
+printtag() {
+    # GitHub Actions tag format
+    echo "::$1::${2-}"
+}
+
+begingroup() {
+    printtag "group" "$1"
+}
+
+endgroup() {
+    printtag "endgroup"
+}
+
+MACPORTS_VERSION=2.8.1
 
 OS_MAJOR=$(uname -r | cut -f 1 -d .)
 OS_ARCH=$(uname -m)
@@ -14,14 +25,30 @@ case "$OS_ARCH" in
         ;;
 esac
 
+MACPORTS_FILENAME=MacPorts-${MACPORTS_VERSION}-${OS_MAJOR}.tar.bz2
 
-echo "::group::Disabling Spotlight"
+
+begingroup "Fetching MacPorts..."
+/usr/bin/curl -fsSLO "https://github.com/macports/macports-ci-files/releases/download/v${MACPORTS_VERSION}/${MACPORTS_FILENAME}" &
+curl_mpbase_pid=$!
+endgroup
+
+
+begingroup "Info"
+echo "macOS version: $(sw_vers -productVersion)"
+echo "IP address: $(/usr/bin/curl -fsS https://www-origin.macports.org/ip.php)"
+/usr/bin/curl -fsSIo /dev/null https://packages-private.macports.org/.org.macports.packages-private.healthcheck.txt && private_packages_available=yes || private_packages_available=no
+echo "Can reach private packages server: $private_packages_available"
+endgroup
+
+
+begingroup "Disabling Spotlight"
 # Disable Spotlight indexing. We don't need it, and it might cost performance
 sudo mdutil -a -i off
-echo "::endgroup::"
+endgroup
 
 
-echo "::group::Uninstalling Homebrew"
+begingroup "Uninstalling Homebrew"
 # Move directories to /opt/off
 echo "Moving directories..."
 sudo mkdir /opt/off
@@ -33,43 +60,40 @@ echo "Removing files..."
 
 # Rehash to forget about the deleted files
 hash -r
-echo "::endgroup::"
+endgroup
 
 
-echo "::group::Installing MacPorts"
-echo "Fetching..."
-# Download resources in background ASAP but use later; do this after cleaning
-# up Homebrew so that we don't end up using their curl!
-curl -fsSLO "https://dl.bintray.com/macports-ci-env/macports-base/MacPorts-${OS_MAJOR}.tar.bz2" &
-curl_mpbase_pid=$!
-
-# Download and install MacPorts built by https://github.com/macports/macports-base/tree/master/.github
+begingroup "Installing MacPorts"
+# Install MacPorts built by https://github.com/macports/macports-base/tree/master/.github
 wait $curl_mpbase_pid
-
 echo "Extracting..."
-sudo tar -xpf "MacPorts-${OS_MAJOR}.tar.bz2" -C /
-rm -f "MacPorts-${OS_MAJOR}.tar.bz2"
-echo "::endgroup::"
+sudo tar -xpf "${MACPORTS_FILENAME}" -C /
+rm -f "${MACPORTS_FILENAME}"
+endgroup
 
 
-echo "::group::Configuring MacPorts"
+begingroup "Configuring MacPorts"
 # Set PATH for portindex
 source /opt/local/share/macports/setupenv.bash
 # CI is not interactive
 echo "ui_interactive no" | sudo tee -a /opt/local/etc/macports/macports.conf >/dev/null
 # Only download from the CDN, not the mirrors
 echo "host_blacklist *.distfiles.macports.org *.packages.macports.org" | sudo tee -a /opt/local/etc/macports/macports.conf >/dev/null
-# Try downloading archives from the private server after trying the public server
-echo "archive_site_local https://packages.macports.org/:tbz2 https://packages-private.macports.org/:tbz2" | sudo tee -a /opt/local/etc/macports/macports.conf >/dev/null
+# Also try downloading archives from the private server
+echo "archive_site_local https://packages-private.macports.org/:tbz2" | sudo tee -a /opt/local/etc/macports/macports.conf >/dev/null
 # Prefer to get archives from the public server instead of the private server
 # preferred_hosts has no effect on archive_site_local
 # See https://trac.macports.org/ticket/57720
 #echo "preferred_hosts packages.macports.org" | sudo tee -a /opt/local/etc/macports/macports.conf >/dev/null
-echo "::endgroup::"
+endgroup
 
 
-echo "::group::Running postflight"
+begingroup "Running postflight"
 # Create macports user
-echo "Postflight..."
 sudo /opt/local/libexec/macports/postflight/postflight
-echo "::endgroup::"
+endgroup
+
+
+begingroup "Updating PortIndex"
+sudo port sync -v
+endgroup
